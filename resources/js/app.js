615dc75b8132038bf11f6dbb6e6558a3247de516
hugoresende27 <Hugoresende27@gmail.com>
2022-02-24 01:04:23 +0000

    l

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+// import * as Vue from 'vue'
+
+import App from '../vue/app' 
+
+const app = new Vue ( {
+    el: '#app',
+    components: { App }
+});
+
+import { createApp } from "vue";
+const app = createApp({
+    // root instance definition
+    el: '#app',
+    components: { App }
+});
